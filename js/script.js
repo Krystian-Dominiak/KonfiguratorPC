cc15bd519a85f586e379952e6e6f85cafe8b54b1
Krystian Dominiak <kdominiak@zsp5kozuchow.pl>
2020-10-08 14:14:35 +0200

    master

--- js/script.js
+++ js/script.js
@@ -2,72 +2,206 @@ const mb = [
     {
         id: 1,
         name: "Gigabyte B450 AORUS ELITE BESTSELLER",
-        price: 409
-
+        price: 409,
+        img:"Gigabyte-B450-Aorus-Elite.jpg"
     },
     {
         id: 2,
         name:"Gigabyte X570 GAMING",
-        price: 769
+        price: 769,
+        img:"Gigabyte-X570-GAMING.jpg"
     },
     {
         id:3,
         name: "Gigabyte GA-A320M-S2H BESTSELLER",
-        price: 239
+        price: 239,
+        img:"Gigabyte-GA-A320M-S2H.jpg"
+    },
+    {
+        id:4,
+        name: "MSI Z390-A PRO ",
+        price: 539,
+        img:"MSI-Z390-A-PRO.jpg"
+    },
+    {
+        id:5,
+        name: "ASRock Fatal1ty B450 GAMING K4",
+        price: 395,
+        img:"ASRock-Fatal1ty-B450-GAMING-K4.jpg"
     }
 ]
 const gfx = [
     {
         id: 1,
         name: "Gigabyte GeForce GTX 1050Ti Windforce OC 4GB GDDR5 (GV-N105TOC-4GD)",
-        price: 639
-
+        price: 639,
+        img:"Gigabyte-GeForce-GTX-1050Ti-Windforce-OC-4GB-GDDR5-(GV-N105TOC-4GD).jpg"
     },
     {
         id: 2,
         name:"Gigabyte GeForce GTX 1660 OC 6GB GDDR5 (GV-N1660OC-6GD)",
-        price: 939
+        price: 939,
+        img:"Gigabyte-GeForce-GTX-1660-OC-6GB-GDDR5-(GV-N1660OC-6GD).jpg"
     },
     {
         id:3,
         name: "MSI GeForce GTX 1650 D6 Gaming X 4GB GDDR6 (GTX 1650 D6 GAMING X)",
-        price: 745
+        price: 745,
+        img:"MSI-GeForce-GTX-1650-D6-Gaming-X-4GB-GDDR6-(GTX-1650-D6-GAMING-X).jpg"
     }
 ]
 const cpu = [
     {
         id: 1,
         name: "Intel Core i5-10400F, 2.9GHz, 12 MB, BOX (BX8070110400F)",
-        price: 699
-
+        price: 699,
+        img:"Intel-Core-i5-10400F-2.9GHz-12-MB-BOX-(BX8070110400F).jpg"
     },
     {
         id: 2,
         name:"Intel Core i7-10700K, 3.8GHz, 16 MB, BOX (BX8070110700K)",
-        price: 1749
+        price: 1749,
+        img:"Intel-Core-i7-10700K-3.8GHz-16 MB-BOX-(BX8070110700K).jpg"
     },
     {
         id:3,
-        name: "AMD Ryzen 5 3600, 3.6GHz, 32 MB, BOX (100-100000031BOX)",
-        price: 929
+        name: "Intel Core i9-10850K, 3.6GHz, 20 MB, BOX (BX8070110850K)",
+        price: 929,
+        img:"Intel-Core-i9-10850K-3.6GHz-20-MB-BOX-(BX8070110850K).jpg"
+    },
+    {
+        id:4,
+        name:"Intel Core i9-10900K, 3.7GHz, 20 MB, BOX (BX8070110900K)",
+        price: 2699,
+        img:"Intel-Core-i9-10900K-3.7GHz-20 MB-BOX-(BX8070110900K).jpg"
+    },
+    {
+        id:5,
+        name:"Intel Core i5-9600K, 3.7GHz, 9 MB, BOX (BX80684I59600K)",
+        price:929,
+        img:"Intel-Core-i5-9600K-3.7GHz-9-MB-BOX-(BX80684I59600K).jpg"
     }
 ]
 const ram = [
     {
         id: 1,
-        name: "Pamięć HyperX Predator RGB, DDR4, 16 GB, 4000MHz, CL19 (HX440C19PB3AK2/16 )",
-        price: 689
-
+        name: "HyperX Predator RGB, DDR4, 16 GB, 4000MHz, CL19 (HX440C19PB3AK2/16)",
+        price: 689,
+        img:"HyperX-Predator-RGB-DDR4-16-GB-4000MHz-CL19-(HX440C19PB3AK2/16).jpg"
     },
     {
         id: 2,
-        name:"Pamięć Corsair Vengeance RGB PRO, DDR4, 16 GB, 3200MHz, CL16 (CMW16GX4M2C3200C16) ",
-        price: 349
+        name:"Corsair Vengeance RGB PRO, DDR4, 16 GB, 3200MHz, CL16 (CMW16GX4M2C3200C16)",
+        price: 349,
+        img:"Corsair-Vengeance-RGB-PRO-DDR4-16-GB-3200MHz-CL16-(CMW16GX4M2C3200C16).jpg"
     },
     {
         id:3,
-        name: "Pamięć HyperX Predator, DDR4, 16 GB, 3200MHz, CL16 (HX432C16PB3K2/16)",
-        price: 355
+        name: "HyperX Predator, DDR4, 16 GB, 3200MHz, CL16 (HX432C16PB3K2/16)",
+        price: 355,
+        img:"HyperX-Predator-DDR4-16-GB-3200MHz-CL16-(HX432C16PB3K2/16).jpg"
+    },
+    {
+        id:4,
+        name: "Patriot Viper 4, DDR4, 8 GB, 3000MHz, CL16 (PV48G300C6K)",
+        price: 160,
+        img:"Patriot-Viper-4-DDR4-8-GB-3000MHz-CL16-(PV48G300C6K).jpg"
+    }
+]
+const zas = [
+    {
+        id: 1,
+        name: "Volcano 750W (ZAS-MC85-SM-750-ATX-VOLCANO)",
+        price: 289,
+        img:"Volcano-750W-(ZAS-MC85-SM-750-ATX-VOLCANO).jpg"
+    },
+    {
+        id: 2,
+        name:"Corsair CX 650M (CP-9020103-EU)",
+        price: 419,
+        img:"Corsair-CX-650M-(CP-9020103-EU).jpg"
+    },
+    {
+        id:3,
+        name: "Chieftec 750W (GDP-750C)",
+        price: 375,
+        img:"Chieftec-750W-(GDP-750C).jpg"
+    },
+    {
+        id:4,
+        name: "Thermaltake Smart 730W (SPS-730MPCBEU)",
+        price: 349,
+        img:"Thermaltake-Smart-730W-(SPS-730MPCBEU).jpg"
+    },
+    {
+        id:5,
+        name: "be quiet! Straight Power 11 650W (BN282)",
+        price: 489,
+        img:"be-quiet!-Straight-Power-11-650W-(BN282).jpg"
+    }
+]
+const obd = [
+    {
+        id: 1,
+        name: "Obudowa MSI MAG Forge 100M",
+        price: 209,
+        img:"Obudowa-MSI-MAG-Forge-100M.jpg"
+    },
+    {
+        id: 2,
+        name:"Genesis Irid 400 ARGB (NPC-1429)",
+        price: 359,
+        img:"Genesis-Irid-400-ARGB-(NPC-1429).jpg"
+    },
+    {
+        id:3,
+        name: "Obudowa Krux Leda (KRX0007)",
+        price: 249,
+        img:"Obudowa-Krux-Leda-(KRX0007).jpg"
+    },
+    {
+        id:4,
+        name: "SilentiumPC Regnum RG6V Evo TG ARGB (SPC262)",
+        price: 319,
+        img:"SilentiumPC-Regnum-RG6V-Evo-TG-ARGB-(SPC262).jpg"
+    },
+    {
+        id:5,
+        name: "Obudowa Nzxt H510I (CA-H510I-W1)",
+        price: 479,
+        img:"Obudowa-Nzxt-H510I-(CA-H510I-W1).jpg"
+    }
+]
+const mon = [
+    {
+        id: 1,
+        name: "Monitor LG 27GL850-B",
+        price: 2149,
+        img:"Monitor-LG-27GL850-B.jpg"
+    },
+    {
+        id: 2,
+        name:"Monitor iiyama G-Master Black Hawk G2530HSU-B1",
+        price: 599,
+        img:"Monitor-iiyama-G-Master-Black-Hawk-G2530HSU-B1.jpg"
+    },
+    {
+        id:3,
+        name: "Monitor Acer ED242QRAbidpx (UM.UE2EE.A01)",
+        price: 668,
+        img:"Monitor-Acer-ED242QRAbidpx-(UM.UE2EE.A01).jpg"
+    },
+    {
+        id:4,
+        name: "Monitor AOC C24G1",
+        price: 849,
+        img:"Monitor-AOC-C24G1.jpg"
+    },
+    {
+        id:5,
+        name: "Monitor AOC 24G2U ",
+        price: 889,
+        img:"Monitor-AOC-24G2U.jpg"
     }
 ]
 var cart = [
@@ -88,6 +222,14 @@ var cart = [
         name: 'ram',
         price:0
     },
+    {
+        name: 'zas',
+        price:0
+    },
+    {
+        name: 'obd',
+        price:0
+    }
 ]
 
 
@@ -110,29 +252,47 @@ function SelectedItemValue(SelectId) {
     const summaryMb = document.getElementById("summaryMb");
     const summaryGfx = document.getElementById("summaryGfx");
     const summaryRam = document.getElementById("summaryRam");
+    const summaryZas = document.getElementById("summaryZas");
+    const summaryObd = document.getElementById("summaryObd");
+    const summaryMon = document.getElementById("summaryMon");
 
     if (SelectId == "cpu") {
-        summaryCPU.innerHTML = cpu[strAtt - 1].name + " " + cpu[strAtt - 1].price
+        summaryCPU.innerHTML = "<td><img src='img/"+cpu[strAtt - 1].img+"'></td>"+"<td></td>"+cpu[strAtt - 1].name + " <td><td> " + cpu[strAtt - 1].price
         cart[2].price = cpu[strAtt - 1].price
         cart[2].name = cpu[strAtt - 1].name
     } else if (SelectId == "mb") {
-        summaryMb.innerHTML = mb[strAtt - 1].name + " " + mb[strAtt - 1].price
+        summaryMb.innerHTML = "<td><img src='img/"+mb[strAtt - 1].img+"'></td>"+"<td></td>"+mb[strAtt - 1].name + " <td><td> " + mb[strAtt - 1].price
         cart[0].price = mb[strAtt - 1].price
         cart[0].name = mb[strAtt - 1].name
         document.getElementById("summaryMainboard").innerHTML
     } else if (SelectId == "gfx") {
-        summaryGfx.innerHTML = gfx[strAtt - 1].name + " " + gfx[strAtt - 1].price
+        summaryGfx.innerHTML = "<td><img src='img/"+gfx[strAtt - 1].img+"'></td>"+"<td></td>"+gfx[strAtt - 1].name + " <td><td> " + gfx[strAtt - 1].price
         cart[1].price = gfx[strAtt - 1].price
         cart[1].name = gfx[strAtt - 1].name
     }
     else if (SelectId == "ram") {
-        summaryRam.innerHTML = ram[strAtt - 1].name + " " + ram[strAtt - 1].price
+        summaryRam.innerHTML = "<td><img src='img/"+ram[strAtt - 1].img+"'></td>"+"<td></td>"+ram[strAtt - 1].name + " <td><td> " + ram[strAtt - 1].price
         cart[3].price = ram[strAtt - 1].price
         cart[3].name = ram[strAtt - 1].name
     }
+    else if (SelectId == "zas") {
+        summaryZas.innerHTML = "<td><img src='img/"+zas[strAtt - 1].img+"'></td>"+"<td></td>"+zas[strAtt - 1].name + " <td><td> " + zas[strAtt - 1].price
+        cart[4].price = zas[strAtt - 1].price
+        cart[4].name = zas[strAtt - 1].name
+    }
+    else if (SelectId == "obd") {
+        summaryObd.innerHTML = "<td><img src='img/"+obd[strAtt - 1].img+"'></td>"+"<td></td>"+obd[strAtt - 1].name + " <td><td> " + obd[strAtt - 1].price
+        cart[5].price = obd[strAtt - 1].price
+        cart[5].name = obd[strAtt - 1].name
+    }
+    else if (SelectId == "mon") {
+        summaryMon.innerHTML = "<td><img src='img/"+mon[strAtt - 1].img+"'></td>"+"<td></td>"+mon[strAtt - 1].name + " <td><td> " + mon[strAtt - 1].price
+        cart[6].price = mon[strAtt - 1].price
+        cart[6].name = mon[strAtt - 1].name
+    }
     else {}
 
-    document.getElementById("sumAll").innerHTML="<b>Suma koszyka:"+ (cart[0].price+cart[1].price+cart[2].price+cart[3].price) + "</b>"
+    document.getElementById("sumAll").innerHTML="<b>Suma koszyka:"+ (cart[0].price+cart[1].price+cart[2].price+cart[3].price+cart[4].price+cart[5].price) + "</b>"
 }
 
 
@@ -149,6 +309,15 @@ function SelectedItemValue(SelectId) {
     for (let i = 0; i < ram.length; i++) {
         addOpt("ram", ram[i].id, ram[i].name)
     }
+    for (let i = 0; i < zas.length; i++) {
+    addOpt("zas", zas[i].id, zas[i].name)
+}
+for (let i = 0; i < obd.length; i++) {
+    addOpt("obd", obd[i].id, obd[i].name)
+}
+for (let i = 0; i < mon.length; i++) {
+    addOpt("mon", mon[i].id, mon[i].name)
+}
 
 
 
